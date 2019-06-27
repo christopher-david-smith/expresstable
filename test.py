80c8ec99cd017339d8122f859299fdd63f0c1ccc
Christopher Smith <christopher.smith@smoothwall.com>
2019-06-27 14:22:49 +0100

    Remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from expresstable import Table, Cell
-
-table = Table(align="center")
-table.add_row([Cell("Name", align="left"), "Age", "Gender"], header=True)
-table.add_row([Cell("Christopher", align="left"), 28, "M"], fgcolor="red")
-table.add_row([Cell("Holly", align="left"), 24, "F"], fgcolor="yellow")
-table.add_row([Cell("Patrick", align="left"), 26, Cell("M", bold=True, bgcolor="white")], fgcolor="green")
-print(table)
